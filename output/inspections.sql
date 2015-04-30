b39398b6cb3d8fd7db06062380fc00ef67015217
Eric Potash <eric@k2co3.net>
2015-04-30 15:12:42 -0500

    output inspections

---
+++ output/inspections.sql
@@ -0,0 +1,10 @@
+drop table if exists output.inspections;
+
+create table output.inspections as (
+	select i.*, 
+	a.id address_id, a.community_area_id, a.ward_id, a.census_tract_id
+	from aux.inspections i
+	join aux.inspection_addresses ia
+	on i.addr_id = ia.inspection_addr_id
+	join aux.addresses a on ia.address_id = a.id
+);
