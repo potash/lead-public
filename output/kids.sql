1caf9d4521b2ce679a0d13cc52aa8e54d80b93de
Eric Potash <eric@k2co3.net>
2015-05-04 19:49:16 -0500

    add min sample date

--- output/kids.sql
+++ output/kids.sql
@@ -17,12 +17,12 @@ select
         date_part('year', t.sample_date) as year,
         t.sample_date - k.date_of_birth test_kid_age_days,
         t.bll test_bll,
+        
         t.minmax test_minmax,
-        t.maxmax test_maxmax,
         t.min test_min,
         t.test_number,
-        k.minmax_test_number,
         k.minmax_bll,
+        k.min_sample_date,
         
         t.address_method,
         st_x(t.geom) address_lat,
