c9ec40502b09caca5ea34f39782d8ee13ba59317
potash <eric@k2co3.net>
2015-05-19 19:22:51 +0000

    fix addresses

--- output/addresses.sql
+++ output/addresses.sql
@@ -2,9 +2,12 @@ DROP TABLE IF EXISTS output.addresses;
 
 CREATE TABLE output.addresses AS (
 
-select a.id address_id, (ta.address_id is not null or ass.residential)::int as residential,
+with test_addresses as (
+    select distinct (address_id) address_id from aux.test_addresses
+)
+
+select a.id address_id, (ta.address_id is not null or ass.residential) as residential,
     a.census_tract_id, a.ward_id
-    from aux.addresses a left join aux.test_addresses ta on a.id = ta.address_id
+    from aux.addresses a left join test_addresses ta on a.id = ta.address_id
     left join aux.assessor_summary ass using(address)
-    where ass.residential
 );
