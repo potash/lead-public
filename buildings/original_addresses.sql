5ad6010d3436c52b91267191b05efee09a96ecc0
potash <eric@k2co3.net>
2015-06-24 21:56:26 +0000

    include id

--- buildings/original_addresses.sql
+++ buildings/original_addresses.sql
@@ -2,7 +2,7 @@ drop table if exists buildings.original_addresses;
 
 create table buildings.original_addresses as (
 	select orig_bldg_, generate_series(f_add1,t_add1,2) || ' ' || pre_dir1 || ' ' || st_name1 || ' ' || st_type1 as address,
-	geom, edit_date
+	geom, edit_date, ogc_fid
 	from buildings.original_buildings
 );
 
--- buildings/original_buildings.sql
+++ buildings/original_buildings.sql
@@ -4,7 +4,7 @@ create temp sequence original_building_id;
 SELECT setval('original_building_id', (select max(orig_bldg_)+1 from input.buildings));
 
 create table buildings.original_buildings as (
-    select coalesce(nullif(orig_bldg_, 0), nextval('original_building_id')) as orig_bldg_, orig_bldg_ as orig_bldg_2,
+    select coalesce(nullif(orig_bldg_, 0), nextval('original_building_id')) as orig_bldg_, orig_bldg_ as orig_bldg_2, ogc_fid,
         f_add1,t_add1, pre_dir1, st_name1, st_type1, edit_date,
         st_transform(st_setsrid(st_point(x_coord,y_coord),3435), 4326) as geom
     from input.buildings
