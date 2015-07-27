324ae9e414c00b864d14293dea028144dec913de
potash <eric@k2co3.net>
2015-07-27 18:43:27 +0000

    python wic agg

--- aux/wic.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists aux.wic;
-
-create table aux.wic as (
-    select first_name,last_name,date_of_birth
-    from input.wic group by first_name, last_name, date_of_birth
-);
-
-alter table aux.wic add column id serial primary key;
