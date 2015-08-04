97eac67b14d12ba72d27b6e311db0fadf32fee79
potash <eric@k2co3.net>
2015-08-04 22:19:49 +0000

    wic pregnant

--- wic/to_csv.sh
+++ wic/to_csv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p $OUTPUT
-rm $OUTPUT/*.txt
+rm $OUTPUT/*.csv
 for filename in $INPUT/*_I.txt; do
     echo $filename
-    clinic=$(basename "$filename" .txt | sed 's/_I$//')
+    clinic=$(basename "$filename" .txt | sed 's/_\(I\|P\)$//')
     $INPUT2 $filename |
     sed 's/[ ]\+,/,/g'  | # right strip fields
     sed 's/,\([0-9]\+\)[ ]\+\([0-9]\+\),,/,\1,\2,/g' | # ugly fix Englewood issues
-    awk -v clinic="$clinic" 'BEGIN {FS = OFS = ","} {NF=13; if (NR != 1) { $13=clinic } else { $13="clinic"; $12="pa_c5";$11="pa_c4";$10="pa_c3";$9="pa_c2";$8="pa_c1"}; print}' > $OUTPUT/$(basename "$filename")
+    awk -v clinic="$clinic" 'BEGIN {FS = OFS = ","} {NF=13; if (NR != 1) { $13=clinic } else { $13="clinic"; $12="pa_c5";$11="pa_c4";$10="pa_c3";$9="pa_c2";$8="pa_c1"}; print}' > $OUTPUT/$(basename "$filename" .txt).csv
 done
