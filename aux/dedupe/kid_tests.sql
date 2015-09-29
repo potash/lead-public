8ea5a969b3bf7cbd794b73da2ac8368f81bc995c
potash <eric@k2co3.net>
2015-09-29 23:55:34 +0000

    dedupe tests

--- aux/dedupe/kid_tests.sql
+++ aux/dedupe/kid_tests.sql
@@ -3,17 +3,22 @@ drop table if exists aux.kid_tests;
 create table aux.kid_tests as (
 
 with kid_tests0 AS (
-	select k.id kid_id, m.id test_id, m.sample_date
-	from aux.kids_initial k join aux.tests m on
-		m.first_name = k.first_name and
-		m.last_name = k.last_name and
-		m.date_of_birth = k.date_of_birth
+	select k.id kid_id, m.id test_id, m.sample_date, 
+            m.sex, m.bll, ta.address_id, m.apt, m.sample_type
+	from aux.kids_initial k join aux.tests m using (first_name, last_name, date_of_birth)
+            left join aux.test_addresses ta on m.id = ta.test_id
 ), 
+
+-- deduplicate tests. do it here because we want to get test_numbers right but we also want to use the deduplicated names and dates of birth
+kid_tests_distinct AS (
+    select kid_id, min(test_id) as test_id, sample_date
+    from kid_tests0 group by kid_id, sample_date, sex, bll, address_id, apt, sample_type
+),
 
 kid_tests1 AS (
 	select kt.test_id, kc.id1 as kid_id, kc.id2 as kid_initial_id,
 		row_number() OVER (PARTITION BY kc.id1 ORDER BY kt.sample_date ASC, kt.test_id asc) as test_number
-	from kid_tests0 kt
+	from kid_tests_distinct kt
 	join aux.kid_components kc on kt.kid_id = kc.id2
 ), 
 
