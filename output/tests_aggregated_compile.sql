ae23c696008917ec382d12c974851fe840d13745
potash <eric@k2co3.net>
2015-10-08 17:03:05 +0000

    tests_aggregated workflow

---
+++ output/tests_aggregated_compile.sql
@@ -0,0 +1,3 @@
+drop table if exists output.tests_aggregated;
+
+alter table output.tests_aggregated_temp rename to tests_aggregated;
---
+++ output/tests_aggregated_init.sql
@@ -0,0 +1,1 @@
+drop table if exists output.tests_aggregated_temp;
