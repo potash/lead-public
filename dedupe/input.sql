73ef3b5cce9ed7623e5239df660f1550c9c9a678
potash <eric@k2co3.net>
2016-01-24 16:03:37 +0000

    dedupe working

--- dedupe/input.sql
+++ dedupe/input.sql
@@ -1,11 +1,11 @@
-CREATE TEMP VIEW dedupe AS ( SELECT true as test, first_name, last_name, mi, date_of_birth, clean_address2 as address, count(*) as count
-FROM aux.tests group by 2,3,4,5,6
+CREATE TEMP VIEW dedupe AS ( SELECT min(id) as test_id, null as cornerstone_id, first_name, last_name, mi, date_of_birth, clean_address2 as address, count(*) as count
+FROM aux.tests group by 3,4,5,6,7
 UNION ALL
-SELECT false as test, brth_fst_t, brth_lst_t, brth_mi_t, birth_d, addr_ln1_t, count(*) as count
+SELECT null, min(part_id_i), brth_fst_t, brth_lst_t, brth_mi_t, birth_d, addr_ln1_t, count(*) as count
 from cornerstone.partenrl 
 join cornerstone.partaddr on part_id_i = addr_id_i
 join cornerstone.partpgm using (part_id_i)
 where category_c='I'
-group by 2,3,4,5,6);
+group by 3,4,5,6,7);
 
 \COPY (select * from dedupe) TO STDOUT WITH CSV HEADER;
