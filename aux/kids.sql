35cbbb95fa2145acb7c159067ce792ee45763375
potash <eric@k2co3.net>
2016-01-25 04:14:03 +0000

    kids

---
+++ aux/kids.sql
@@ -0,0 +1,25 @@
+drop table if exists aux.kids;
+
+create table aux.kids as (
+
+select kid_id as id, mode(first_name) as first_name, mode(last_name) as last_name,
+
+-- take the average date of birth (postgresql doesn't have avg(date), so cast to epoch and back)
+-- if a birth date doesn't make sense, exclude it from the average
+to_timestamp(mode(
+	CASE WHEN t.date_of_birth < t.sample_date THEN extract(epoch from t.date_of_birth) ELSE null END
+))::date as date_of_birth,
+
+count(distinct test_id) as num_tests,
+
+min(t.sample_date) as min_sample_date,
+max(t.sample_date) as max_sample_date,
+
+max(t.bll) as max_bll
+
+from aux.kid_tests kt
+join aux.tests t on kt.test_id = t.id
+group by kt.kid_id
+);
+
+alter table aux.kids add primary key (id);
