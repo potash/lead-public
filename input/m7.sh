9dc9570e7ad42851e2c32635654c6aa00a0fd470
potash <eric@k2co3.net>
2016-02-01 06:42:12 +0000

    geocoded m7

--- input/m7.sh
+++ input/m7.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-file=$1
 
 psql -c "
     DROP TABLE IF EXISTS input.m7; 
 
     CREATE TABLE input.m7 (cleaned_address text, 
         xcoord text, ycoord text,
-        id integer primary key, first_name text, last_name text, sex char,
-        date_of_birth date, address text, apt text, city text,
-        bll int, sample_type char, sample_date date, data_source integer, lab text,
-        dbod date, birthcheck integer);"
+        id text, first_name text, last_name text, sex char,
+        date_of_birth text, address text, apt text, city text,
+        bll text, sample_type char, sample_date text, data_source integer, lab text,
+        dbod text, birthcheck text, 
+        geocode_full_addr text, geocode_house_low text, geocode_house_high text,
+        geocode_pre text, geocode_street_name text, geocode_street_type text, geocode_sufdir text, geocode_xcoord text, geocode_ycoord text, geocode_status1 text, geocode_status2 text);"
 
-PGCLIENTENCODING="latin1" \
-psql -c \
-"\COPY input.m7 FROM '$file' WITH CSV HEADER;"
+cat "$1" | PGCLIENTENCODING="latin1" psql -c "\COPY input.m7 FROM STDIN WITH CSV HEADER;"
