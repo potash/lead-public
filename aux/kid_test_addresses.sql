e6e08ae37c50da1506a5619d0762beb5714d43be
potash <eric@k2co3.net>
2016-02-02 17:58:41 +0000

    use new dedupe

--- aux/kid_test_addresses.sql
+++ aux/kid_test_addresses.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS aux.kid_test_addresses;
 CREATE TABLE aux.kid_test_addresses AS (
 
 select kid_id, address_id, 
-min(test_date) AS min_date, 
-max(test_date) as max_date, 
+min(sample_date) AS min_date, 
+max(sample_date) as max_date, 
 count(*) AS num_tests
 
 FROM output.tests
--- aux/kids.sql
+++ aux/kids.sql
@@ -2,24 +2,13 @@ drop table if exists aux.kids;
 
 create table aux.kids as (
 
-with test_kids as (
-    select kid_id, first_name, last_name, 
-    CASE WHEN date_of_birth < sample_date THEN date_of_birth ELSE null END AS date_of_birth
-    from aux.kid_tests_info join aux.tests t on test_id = id
-),
-
-wic_kids as (
-    select kid_id, brth_fst_t first_name, brth_lst_t last_name, birth_d date_of_birth
-    from aux.kid_wics join cornerstone.partenrl using (part_id_i)
-)
-
 select kid_id, mode(first_name) as first_name, mode(last_name) as last_name,
 
 -- take the average date of birth (postgresql doesn't have avg(date), so cast to epoch and back)
 -- if a birth date doesn't make sense, exclude it from the average
-to_timestamp(mode(extract(epoch from date_of_birth)))::date as date_of_birth
+to_timestamp(mode(extract(epoch from date_of_birth::date)))::date as date_of_birth
 
-from (select * from test_kids UNION ALL select * from wic_kids) t
+from dedupe.infants join dedupe.entity_map using (id)
 group by kid_id
 );
 
