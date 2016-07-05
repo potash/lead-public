7f0d7cab7c0a401a1d7f8cbd8f95205791d6ad63
potash <eric@k2co3.net>
2016-07-05 19:26:51 +0000

    labs and tests

--- output/tests.py
+++ output/tests.py
@@ -9,10 +9,6 @@ import pandas as pd
 import logging
 
 # TODO: make this more efficient by not including unnecessary address columns
-tests = FromSQL(table='output.tests', parse_dates=['date'], 
-        target=True)
-addresses = FromSQL(table='output.addresses', target=True)
-
 class TestsAggregation(SpacetimeAggregation):
     def __init__(self, spacedeltas, dates, **kwargs):
         SpacetimeAggregation.__init__(self,
@@ -20,7 +16,9 @@ class TestsAggregation(SpacetimeAggregation):
             date_column='date', **kwargs)
 
         if not self.parallel:
-            self.inputs = [Merge(inputs=[tests, addresses], on='address_id')]
+            self.inputs = [Merge(inputs=[
+                    FromSQL(table='output.tests', parse_dates=['date'], target=True), 
+                    FromSQL(table='output.addresses', target=True)], on='address_id')]
 
     def get_aggregates(self, date, delta):
         kid_count = Aggregate('kid_id', 'nunique', 
@@ -29,6 +27,7 @@ class TestsAggregation(SpacetimeAggregation):
         aggregates = [
             Count(),
             Aggregate('bll', ['mean', 'median', 'max', 'min', 'std']),
+            Aggregate(lambda t: t.bll.where(t.increase), ['mean', 'median', 'max', 'min', 'std'], 'increase_bll'),
             Count(lambda t: t.bll <= 2, 'bll2', prop=True),
             Fraction(Count(['first_bll6', 'first_bll10']), kid_count, 
                     include_numerator=True, include_denominator=True),
