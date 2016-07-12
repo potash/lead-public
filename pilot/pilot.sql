708455d5fd42b664bf8a297e3ee3df2e5333cc30
potash <eric@k2co3.net>
2016-07-12 20:04:44 +0000

    test addresses and address flags

--- pilot/pilot.sql
+++ pilot/pilot.sql
@@ -1,15 +1,21 @@
 -- wic_predictions summarizes the cornerstone wic data for a given kid_id
 create temp table wic_contact as (
-with wic_address_ids as (
-    select kid_id, address_id, true as infant, 
-    unnest(wic_infant_ogc_fids) ogc_fid
-    from predictions
+with highest_risk as (
+    select distinct on (kid_id) kid_id, address_id
+    from predictions order by kid_id, score desc
+),
+
+wic_address_ids as (
+    select kid_id, address_id,
+        wic_infant_ogc_fids is not null as address_wic_infant,
+        wic_mother_ogc_fids is not null as address_wic_mother,
+        p.address_test_min_date is null as address_test,
+        unnest(coalesce(wic_infant_ogc_fids || wic_mother_ogc_fids, 
+                '{NULL}'::integer[])) ogc_fid,
+        highest_risk.address_id is not null as address_primary
+    from predictions p
     join output.kid_addresses using (kid_id, address_id)
-    UNION ALL
-    select kid_id, address_id, false as infant,
-    unnest(wic_mother_ogc_fids) ogc_fid
-    from predictions
-    join output.kid_addresses using (kid_id, address_id)
+    left join highest_risk using (kid_id, address_id)
 )
 
 select kid_id, address_id, address,
@@ -18,7 +24,7 @@ select kid_id, address_id, address,
     array_remove(array_agg(distinct trim(addr_apt_t)), null) addr_apt_t,
     array_remove(array_agg(distinct addr_zip_n), null) addr_zip_n,
     array_remove(array_agg(distinct 
-        CASE WHEN infant THEN trim(
+        CASE WHEN address_wic_infant THEN trim(
             -- change 'DOE, JANE' to 'JANE DOE'
             CASE WHEN position(',' in cont_nme_t) > 0 THEN
                 substring(cont_nme_t from position(',' in cont_nme_t) + 1) 
@@ -29,13 +35,18 @@ select kid_id, address_id, address,
         ELSE brth_fst_t || ' ' || brth_lst_t END
     ), null) cont_nme_t,
     array_remove(array_agg(distinct 
-        CASE WHEN infant THEN relate_c ELSE 'MO' END), null) relate_c,
+        CASE WHEN address_wic_infant 
+             THEN relate_c ELSE 'MO' END), null) relate_c,
     array_remove(array_agg(distinct nullif(phne_nbr_n, 0)), null) phne_nbr_n,
-    array_remove(array_agg(distinct nullif(cell_nbr_n, 0)), null) cell_nbr_n
+    array_remove(array_agg(distinct nullif(cell_nbr_n, 0)), null) cell_nbr_n,
+    bool_or(address_wic_infant) as address_wic_infant,
+    bool_or(address_wic_mother) as address_wic_mother,
+    bool_or(address_test) as address_test,
+    bool_or(address_primary) as address_primary
 from wic_address_ids
 join aux.addresses using (address_id)
-join cornerstone.partaddr using (ogc_fid)
-join cornerstone.partenrl on addr_id_i = part_id_i
+left join cornerstone.partaddr using (ogc_fid)
+left join cornerstone.partenrl on addr_id_i = part_id_i
 group by kid_id, address_id, address
 );
 
@@ -90,7 +101,7 @@ create table pilot01 as
 
 drop table if exists pilot01_contact;
 create table pilot01_contact as (
-    select part_id_i, first_name, last_name, date_of_birth, inspection, wic_contact.* 
+    select part_id_i, first_name, last_name, date_of_birth, inspection, wic_contact.*
     from pilot01 join wic_contact using (kid_id)
 );
 
