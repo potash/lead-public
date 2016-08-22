a7c5f3daee86bca8a98beabb82a99fe2b0db9a82
potash <eric@k2co3.net>
2016-08-22 03:51:58 +0000

    labs

--- output/labs.sql
+++ output/labs.sql
@@ -2,22 +2,29 @@ drop table if exists output.labs;
 
 create table output.labs as (
 with lab_blls as (select lab_id, 
---        extract(year from sample_date) as year,
+        --extract(year from sample_date) as year,
         bll, count(*) 
-    from aux.tests group by 1,2),
+    from aux.tests group by 1,2
+    --,3
+    ),
 
 lab_modes as (
 
-select distinct on (lab_id--, year
-    ) lab_id, --year, 
-    bll as mode_bll from lab_blls order by lab_id, --year, 
+select distinct on (lab_id
+    --, year
+    ) lab_id, 
+    --year, 
+    bll as mode_bll from lab_blls order by lab_id, 
+    --year, 
     count desc),
 
 lab_counts as (
     select lab_id, 
     --extract(year from sample_date) as year, 
-    count(*) as test_count from aux.tests group by 1--,2
+    count(*) as test_count from aux.tests group by 1
+    --,2
 )
 
-select * from lab_modes join lab_counts using (lab_id --, year
+select * from lab_modes join lab_counts using (lab_id
+--, year
 ));
