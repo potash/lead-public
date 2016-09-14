60fab6c0b0d74cd179ab5e8ec877ff62ce2ec001
potash <eric@k2co3.net>
2016-09-14 15:47:44 +0000

    incidences

--- output/tests.py
+++ output/tests.py
@@ -13,12 +13,20 @@ class TestsAggregation(SpacetimeAggregation):
     def __init__(self, spacedeltas, dates, **kwargs):
         SpacetimeAggregation.__init__(self,
             spacedeltas=spacedeltas, dates=dates, prefix='tests',
-            date_column='date', **kwargs)
+            date_column='date', censor_columns={'first_bll6_sample_date':[], 'first_bll10_sample_date':[]}, **kwargs)
 
         if not self.parallel:
             self.inputs = [Merge(inputs=[
-                    FromSQL(table='output.tests', parse_dates=['date'], target=True), 
-                    FromSQL(table='output.addresses', target=True)], on='address_id')]
+                    Merge(inputs=[
+                        FromSQL(table='output.tests', parse_dates=['date'], target=True), 
+                        FromSQL(table='output.addresses', target=True)], on='address_id'),
+                    # get kid first bll6 and bll10 counts to calculate incidences
+                    FromSQL("""
+                    select kid_id, first_bll6_sample_date, first_bll10_sample_date 
+                    from output.kids
+                    """, parse_dates=['first_bll6_sample_date', 'first_bll10_sample_date'])],
+                on='kid_id', target=True
+            )]
 
     def get_aggregates(self, date, delta):
         kid_count = Aggregate('kid_id', 'nunique', 
@@ -29,9 +37,26 @@ class TestsAggregation(SpacetimeAggregation):
             Aggregate('bll', ['mean', 'median', 'max', 'min', 'std']),
             Aggregate(lambda t: t.bll.where(t.increase), ['mean', 'median', 'max', 'min', 'std'], 'increase_bll'),
             Count(lambda t: t.bll <= 2, 'bll2', prop=True),
+            # prevalences
             Fraction(Count(['first_bll6', 'first_bll10']), kid_count, 
                     include_numerator=True, include_denominator=True),
         ]
+
+        # incidences
+        if delta != 'all':
+            start_date = date - data.parse_delta(delta)
+            no_bll6_count = Aggregate(lambda k: k.kid_id.where((k.first_bll6_sample_date >= start_date).fillna(True)), 
+                    'nunique', name='no_bll6_count', fname=False)
+            no_bll10_count = Aggregate(lambda k: k.kid_id.where((k.first_bll10_sample_date >= start_date).fillna(True)), 
+                    'nunique', name='no_bll10_count', fname=False)
+
+            aggregates.extend([
+                no_bll6_count, 
+                no_bll10_count,
+                Count('first_bll6')/no_bll6_count, 
+                Count('first_bll10')/no_bll10_count])
+
+
         if delta == 'all':
             aggregates.extend([
                 Aggregate(days('date',date), ['min','max'], 
