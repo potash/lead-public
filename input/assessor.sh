f4c72dbe57a4307af5847258b09399e0dd061028
Eric Potash <eric@k2co3.net>
2016-09-29 23:19:28 +0000

    drop assessor before import

--- input/assessor.sh
+++ input/assessor.sh
@@ -3,7 +3,7 @@
 file=$1
 
 # create table in input schema, call it assessor, replace col spaces with underscores
-mdb-schema -N input $file postgres | 
+mdb-schema --drop-table -N input $file postgres | 
 	sed 's/Cookcounty land bank14/assessor/' | 
 	awk -F\" '{OFS="\"";for(i=2;i<NF;i+=2)gsub(/ /,"_",$i);print}' | 
 	psql
