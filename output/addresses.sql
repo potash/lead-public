bcabdc87711a4ed5f12c94584adedb0d8cc0c50c
Eric Potash <eric@k2co3.net>
2016-09-30 21:17:10 +0000

    addresses from investigations

--- output/addresses.sql
+++ output/addresses.sql
@@ -7,7 +7,7 @@ with residential_addresses as (
     UNION
     select address_id from aux.kid_wic_addresses
     UNION
-    select address_id from output.inspections
+    select address_id from output.investigations
 ),
 
 residential_complexes as (
@@ -15,7 +15,7 @@ residential_complexes as (
     from aux.addresses a 
     left join aux.complex_addresses ca using (address_id)
     left join aux.assessor using (address)
-    group by complex_id having bool_or(residential)
+    group by complex_id having bool_or(residential > 0)
 )
 
 select a.address_id address_id, a.address, ca.building_id, ca.complex_id,
