eedde196193000c4e343bd42c17da76464887e92
Eric Potash <eric@k2co3.net>
2017-03-28 18:10:54 +0000

    use db

--- lead/input/acs/import.py
+++ lead/input/acs/import.py
@@ -4,4 +4,4 @@ import sys
 
 acs = pd.read_csv(sys.argv[1], dtype= {'census_tract_id':float})
 db = util.create_db()
-db.to_sql(acs, name='acs', con=engine, schema='input', if_exists='replace', index=False)
+db.to_sql(acs, name='acs', schema='input', if_exists='replace', index=False)
