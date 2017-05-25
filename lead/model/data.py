5458e2bc733f959bf1923cb52d68efd424c0d9e4
Eric Potash <eric@k2co3.net>
2017-05-25 19:35:05 +0000

    dtype

--- lead/model/data.py
+++ lead/model/data.py
@@ -13,16 +13,19 @@ import numpy as np
 import logging
 
 class LeadData(Step):
-    def __init__(self, month, day, year_min, year_max, wic_lag=None):
+    def __init__(self, month, day, year_min, year_max, wic_lag=None, dtype=None):
+        if dtype is None:
+            dtype = np.float16
+
         Step.__init__(self, month=month, day=day, 
                 year_min=year_min, year_max=year_max,
-                wic_lag=wic_lag)
+                wic_lag=wic_lag, dtype=dtype)
 
         left = LeadLeft(month=month, day=day, year_min=year_min)
         left.target = True
 
         acs = Call("astype", inputs=[ACS(inputs=[left])], 
-                   dtype=np.float16)
+                   dtype=dtype)
         acs.target = True
 
         dates = tuple((date(y, month, day) for y in range(year_min, year_max+1)))
@@ -34,7 +37,7 @@ class LeadData(Step):
                     inputs=[left, a], 
                     lag=wic_lag if name.startswith('wic') else None,
                     inputs_mapping=[{'aux':None}, 'aggregation'])
-            aj = Call("astype", inputs=[aj], dtype=np.float16)
+            aj = Call("astype", inputs=[aj], dtype=dtype)
             aj.target = True
             self.aggregation_joins.append(aj)
 
@@ -44,18 +47,21 @@ class LeadData(Step):
     def run(self, acs, left, aux):
         # join all aggregations
         logging.info('Joining aggregations')
+        index_columns = ['kid_id','address_id','date']
+        left = left[index_columns]
+
         X = left.join([a.get_result() for a in self.aggregation_joins] + [acs])
         # delete all aggregation inputs so that memory can be freed
         for a in self.aggregation_joins: del a._result
 
         logging.info('Dates')
-        X['age'] = (aux.date - aux.date_of_birth)/util.day
-        X['date_of_birth_days'] = util.date_to_days(aux.date_of_birth)
-        X['date_of_birth_month'] = aux.date_of_birth.dt.month
-        X['wic'] = (aux.first_wic_date < aux.date).fillna(False)
+        X['age'] = ((aux.date - aux.date_of_birth)/util.day).astype(self.dtype)
+        X['date_of_birth_days'] = util.date_to_days(aux.date_of_birth).astype(self.dtype)
+        X['date_of_birth_month'] = aux.date_of_birth.dt.month.astype(self.dtype)
+        X['wic'] = (aux.first_wic_date < aux.date).fillna(False).astype(self.dtype)
 
-        X.set_index(['kid_id', 'address_id', 'date'], inplace=True)
-        aux.set_index(['kid_id', 'address_id', 'date'], inplace=True)
+        X.set_index(index_columns, inplace=True)
+        aux.set_index(index_columns, inplace=True)
 
         c = data.non_numeric_columns(X)
         if len(c) > 0:
